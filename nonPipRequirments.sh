4532a31c903611fa95c1c2e89d0433c8c85b75f2
phelimb <wave@phel.im>
2013-12-11 12:23:03 +0000

    rm folder

--- nonPipRequirments.sh
+++ nonPipRequirments.sh
@@ -1,6 +1,2 @@
-wget -O bwa-0.7.5.tar.bz2 http://sourceforge.net/projects/bio-bwa/files/bwa-0.7.5a.tar.bz2/download
-tar -xvf bwa-0.7.5.tar.bz2 
-rm bwa-0.7.5.tar.bz2 
-cd bwa-0.7.5a
-make
-cp bwa /usr/local/bin
+#!/usr/bin/env sh
+wget -O bwa-0.7.5.tar.bz2 http://sourceforge.net/projects/bio-bwa/files/bwa-0.7.5a.tar.bz2/download && tar -xvf bwa-0.7.5.tar.bz2 && rm bwa-0.7.5.tar.bz2 && cd bwa-0.7.5a && make && cp bwa /usr/local/bin && cd .. && rm -r bwa-0.7.5a
